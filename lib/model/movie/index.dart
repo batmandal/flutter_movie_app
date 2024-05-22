37e4b3fd9f257e9e0648e1cebb8e83bd07040b29
batmandal <batmandal98832221@gmail.com>
2024-05-22 09:51:02 +0800

    install some packages:

--- lib/model/movie/index.dart
+++ lib/model/movie/index.dart
@@ -1,1 +1,25 @@
-class MovieModel {}
+import "package:json_annotation/json_annotation.dart";
+
+part 'index.g.dart';
+
+@JsonSerializable()
+class MovieModel {
+  String id;
+  String imgUrl;
+  int? publishedYear, durationMin;
+  String? type, description;
+
+  MovieModel(
+      {required this.id,
+      required this.imgUrl,
+      this.description,
+      this.durationMin,
+      this.publishedYear,
+      this.type});
+
+  factory MovieModel.fromJson(Map<String, dynamic> json) =>
+      _$MovieModelFromJson(json);
+
+  /// Connect the generated [_$MovieModelToJson] function to the `toJson` method.
+  Map<String, dynamic> toJson() => _$MovieModelToJson(this);
+}
